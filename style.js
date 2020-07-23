3060735faffb7167f923caa9891bb76e88c097d9
Lsong <song940@gmail.com>
2020-07-23 21:16:17 +0800

    add style

---
+++ style.js
@@ -0,0 +1,11 @@
+
+const uuid = () =>
+  (Math.random() * 1e9 | 0).toString(16)
+
+export const css = str => {
+  const className = 'stylesheet-' + uuid();
+  const style = document.createElement('style');
+  style.textContent = str.replace(/:host/, `.${className}`);
+  document.head.appendChild(style);
+  return className;
+};
