570ec4a6dcb558a338485b0904199f132191c7bd
Lsong <song940@gmail.com>
2020-09-01 13:15:46 +0800

    Add File API: readFileAsText

---
+++ file.js
@@ -0,0 +1,7 @@
+
+export const readFileAsText = file => new Promise((resolve, reject) => {
+  const reader = new FileReader();
+  addEventListener(reader, 'error', reject);
+  addEventListener(reader, 'load', () => resolve(reader.result));
+  return reader.readAsText(file);
+});
