44e9d4c6279d098453608cf62156484967591ad5
Lsong <song940@gmail.com>
2020-09-01 11:16:53 +0800

    allow addEventListener use el as selector

--- dom.js
+++ dom.js
@@ -7,12 +7,15 @@ export const querySelectorAll = (selector, el = document) =>
   el.querySelectorAll(selector);
 
 export const addEventListener = (el, type, fn, options) => {
+  if (typeof el === 'string') el = querySelector(el);
   el.addEventListener(type, fn, options);
   return () => removeEventListener(el, type, fn, options);
 };
 
-export const removeEventListener = (el, type, fn, options) =>
+export const removeEventListener = (el, type, fn, options) => {
+  if (typeof el === 'string') el = querySelector(el);
   el.removeEventListener(type, fn, options);
+}
 
 export const ready = fn => {
   if (/loaded|complete/.test(document.readyState))
