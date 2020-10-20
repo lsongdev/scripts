06d266feea5a0b96d46b22591938a083ff1c081b
Lsong <song940@gmail.com>
2020-10-20 19:00:48 +0800

    add device orientation

--- example/sensor/index.js
+++ example/sensor/index.js
@@ -1,5 +1,5 @@
 import { ready, querySelector as $, addEventListener as on } from '../../dom.js';
-import { accelerometer } from '../../sensor.js';
+import { accelerometer, orientation } from '../../sensor.js';
 
 ready(() => {
   const acl = accelerometer({}, acl => {
@@ -9,5 +9,34 @@ ready(() => {
   });
   on('#accelerometer_start', 'click', () => {
     acl.start();
+  });
+
+  const ball = $('.ball');
+  const garden = $('.garden');
+  const output = $('.output');
+
+  const maxX = garden.clientWidth - ball.clientWidth;
+  const maxY = garden.clientHeight - ball.clientHeight;
+
+  orientation(({ absolute, alpha, beta: x, gamma: y }) => {
+
+    output.innerHTML = "beta : " + x + "\n";
+    output.innerHTML += "gamma: " + y + "\n";
+
+    // Because we don't want to have the device upside down
+    // We constrain the x value to the range [-90,90]
+    if (x > 90) { x = 90 };
+    if (x < -90) { x = -90 };
+
+    // To make computation easier we shift the range of 
+    // x and y to [0,180]
+    x += 90;
+    y += 90;
+
+    // 10 is half the size of the ball
+    // It center the positioning point to the center of the ball
+    ball.style.top = (maxY * y / 180 - 10) + "px";
+    ball.style.left = (maxX * x / 180 - 10) + "px";
+
   });
 });
--- sensor.js
+++ sensor.js
@@ -1,6 +1,11 @@
+import { addEventListener } from './dom.js';
 
 export const accelerometer = (options = { frequency: 60 }, cb) => {
   const acl = new Accelerometer(options);
   acl.addEventListener('reading', () => cb(acl));
   return acl;
+};
+
+export const orientation = cb => {
+  return addEventListener(window, 'deviceorientation', cb, true);
 };
