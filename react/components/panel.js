e7d3d50791528900dcc5bcdca02fcaf86f9ef745
Lsong <song940@gmail.com>
2024-07-24 22:11:56 +0800

    update

--- react/components/panel.js
+++ react/components/panel.js
@@ -3,7 +3,10 @@ import { cls } from '../../dom.js';
 
 export const Panel = ({ className = '', title, header, footer, children }) => {
   return h('div', { className: cls('panel', className) },
-    h('div', { className: 'panel-header' }, [title, header]),
+    h('div', { className: 'panel-header' }, [
+      title && h('span', { className: 'panel-title' }, title), 
+      header
+    ]),
     h('div', { className: 'panel-body' }, children),
     footer && h('div', { className: 'panel-footer' }, footer),
   );
