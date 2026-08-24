864554347cb1cf34946328d6a95407dfb316085a
Lsong <song940@gmail.com>
2026-08-25 01:23:12 +0800

    update

---
+++ streams/index.js
@@ -0,0 +1,66 @@
+
+
+export const createTextReader = stream => {
+  const decoder = new TextDecoderStream();
+  stream.pipeTo(decoder.writable);
+  const inputStream = decoder.readable;
+  return inputStream.getReader();
+};
+
+export const createTextWriter = stream => {
+  const encoder = new TextEncoderStream();
+  encoder.readable.pipeTo(stream);
+  const outputStream = encoder.writable;
+  return outputStream.getWriter();
+};
+
+export const readText = stream => {
+  const reader = createTextReader(stream);
+  const lines = [];
+  for (const value of reader.read()) {
+    lines.push(value);
+  }
+  reader.releaseLock();
+  return lines;
+};
+
+export const writeText = (stream, lines) => {
+  const writer = createTextWriter(stream);
+  lines.forEach(line => writer.write(line));
+  writer.releaseLock();
+  return writer;
+};
+
+export async function* readStream(reader) {
+  let value, done = false;
+  do {
+    ({ value, done } = await reader.read());
+    yield value;
+  } while (!done);
+}
+
+export async function* readLines(reader) {
+  let buffer = '';
+  const decoder = new TextDecoder('utf-8');
+  for await (let value of readStream(reader)) {
+    if (typeof value !== 'string')
+      value = decoder.decode(value, { stream: true });
+    buffer += value;
+    const lines = buffer.split('\n');
+    buffer = lines.pop();
+    for (const line of lines) {
+      if (line.trim()) {
+        yield line;
+      }
+    }
+  }
+  if (buffer.trim()) {
+    yield buffer;
+  }
+}
+
+export async function* parseJSONLines(reader) {
+  for await (const line of readLines(reader)) {
+    yield JSON.parse(line);
+  }
+}
